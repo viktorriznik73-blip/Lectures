3e8a1167d75fd18d23c941dfb4e2b98fd8e67e14
viktorriznik73-blip <viktorriznik73@gmail.com>
2026-08-04 14:27:47 +0200

    Start learning TypeScript

--- TypeScript-learning/typescript.js
+++ TypeScript-learning/typescript.js
@@ -1,3 +1,6 @@
 "use strict";
-const color = { hex: "#187ABF" };
-console.log(color);
+function logPoint(p) {
+    console.log(`${p.x}, ${p.y}`);
+}
+const point = { x: 15, y: 30 };
+logPoint(point);
--- TypeScript-learning/typescript.ts
+++ TypeScript-learning/typescript.ts
@@ -1,3 +1,9 @@
-
-const color = { hex: "#187ABF"}
-console.log(color)
+interface Point {
+    x: number;
+    y: number;
+}
+function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+const point = {x: 15, y: 30};
+logPoint(point);
