1dbf825a7761f52f256117c6700b6e4e7c9f145a
viktorriznik73-blip <viktorriznik73@gmail.com>
2026-08-05 18:42:40 +0200

    Working with types, useCallback, createContext, useContext, useState, useMemo, useReducer

--- TypeScript-learning/my-react-app/src/App.tsx
+++ TypeScript-learning/my-react-app/src/App.tsx
@@ -1,6 +1,132 @@
-import { useReducer } from 'react';
+import React from 'react';
+import './App.css'
+// React.ReactNode accepts the most inputs
+interface ReactNodeProps {
+  children: React.ReactNode;
+}
+const RNode = (props: ReactNodeProps) => <div>{props.children}</div>
+
+const ReactNodeApp = () => <>
+<RNode><p>One element</p></RNode>
+<RNode>
+  <>
+  <p>Fragments for</p>
+  <p>More elements</p>
+  </>
+</RNode>
+<RNode>1</RNode>
+<RNode>Hello</RNode>
+<RNode>{true}</RNode>
+<RNode>{null}</RNode>
+
+
+<RNode />
+</>
+// React.ReactElement accepts only JSX elements
+interface ReactElementProps {
+  children: React.ReactElement;
+}
+
+const RElement = (props: ReactElementProps) => <div>{props.children}</div>
+
+const ReactElementApp = () => <>
+<RElement><p>More elements</p></RElement>
+<RElement>
+  <>
+  <p>More elements</p><p>More elements</p>
+  </>
+</RElement>
+
+<RElement>1</RElement>
+<RElement>Hello</RElement>
+<RElement>{true}</RElement>
+<RElement>{null}</RElement>
+<RElement />
+</>
+/*
+useCallback
+export default function Form() {
+  const [value, setValue] = useState("Change me!");
+
+  const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>((event) => {
+setValue(event.currentTarget.value)
+  }, [setValue])
+
+  return (
+    <>
+      <input type="text" value={value} onChange={handleChange}/>
+      <p>Value: {value}</p>
+    </>
+  )
+}
+  */
+ /* 
+ import { createContext, useContext, useState, useMemo } from 'react';
+ useMemo and useContext
 import './App.css'
 
+
+type ComplexObject = {
+  kind: string
+}
+const Context = createContext<ComplexObject | null>(null);
+
+const useGetComplexObject = () => {
+  const object = useContext(Context)
+  if (!object) { throw new Error("useGetComplexObject must be used within a Provider") }
+  return object
+}
+
+export default function App() {
+  const object = useMemo(() => ({ kind: "complex"}), [])
+
+  return (
+    <Context value={object}>
+       <MyComponent />
+    </Context>
+  )
+}
+  function MyComponent() {
+    const object = useGetComplexObject();
+
+    return (
+<div>
+  <p>Current object {object.kind}</p>
+</div>
+    )
+  } 
+    */
+   /*
+    import './App.css'
+useContext
+type Theme = | "dark" | "light" | "system";
+const ThemeContext = createContext<Theme>("system")
+
+const useGetTheme = () => useContext(ThemeContext);
+
+export default function App() {
+  const [theme, setTheme] = useState<Theme>('dark');
+
+  return (
+<ThemeContext value={theme} >
+<MyComponent />
+</ThemeContext>
+  )
+  function MyComponent() {
+    const theme = useGetTheme()
+  
+  return (
+    <div>
+      <p>Current theme: {theme}</p>
+    </div>
+  )
+}
+}
+*/
+/*
+import { useReducer } from 'react';
+import './App.css'
+useReducer
 interface State {
   count: number
 };
@@ -34,3 +160,4 @@ export default function App() {
 </div>
   )
 }
+*/
--- TypeScript-learning/my-react-app/src/main.tsx
+++ TypeScript-learning/my-react-app/src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MyApp from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MyApp />
   </StrictMode>,
 )
